ba9a699ac404e25b2e42e9767464bf8938225492
freegoleet <rorocoffeee@gmail.com>
2020-09-27 17:28:41 +0200

    Moving stuff

---
+++ Waypoint System/Assets/Editor/WaypointsEditor.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(Waypoints))]
+[CanEditMultipleObjects]
+public class WaypointsEditor : Editor
+{
+    SerializedProperty waypoints;
+    SerializedProperty waypoint;
+
+    public void OnEnable()
+    {
+        waypoints = serializedObject.FindProperty("waypoints");
+        waypoint = serializedObject.FindProperty("waypoint");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(waypoints);
+        Waypoints waypointsRef = (Waypoints)target;
+        EditorGUILayout.PropertyField(waypoint);
+
+        waypointsRef.speed = EditorGUILayout.Slider("Speed", waypointsRef.speed, 0, 100);
+
+        if (GUILayout.Button("Add Waypoint"))
+        {
+            waypointsRef.AddNewWaypoint();
+        }
+        
+
+        if (GUI.changed && !Application.isPlaying)
+        {
+            EditorUtility.SetDirty(waypointsRef);
+            EditorSceneManager.MarkSceneDirty(waypointsRef.gameObject.scene);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void OnSceneGUI()
+    {
+
+        for (int i = 0; i < waypoints.arraySize; i++)
+        {
+            var t = (target as Waypoints);
+            EditorGUI.BeginChangeCheck();
+            Vector3 pos = Handles.PositionHandle(t.waypoints[i].transform.position, Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RegisterFullObjectHierarchyUndo(t.waypoints[i], "Move point");
+                if (pos != null)
+                {
+                    t.waypoints[i].transform.position = pos;
+                    t.Update();
+                }
+            }
+        }
+    }
+}
---
+++ Waypoint System/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[System.Serializable]
+public class Waypoints : MonoBehaviour
+{
+    public List<GameObject> waypoints = new List<GameObject>();
+    public GameObject waypoint;
+    private bool play = true;
+    private bool repeat = true;
+    //private bool pause = false;
+
+    public float speed = 5f;
+
+    private float fraction = 0;
+    private float distance = 0;
+    private int points = 0;
+
+    private Vector3 nextPos;
+    private Vector3 currentPos;
+
+    private GameObject sphere = null;
+
+    public void Update() {
+        LerpEntity();
+    }
+
+    public void AddNewWaypoint()
+    {
+        GameObject newWaypoint = Instantiate(waypoint, waypoints[waypoints.Count() - 1].transform.position, waypoints[waypoints.Count() - 1].transform.rotation, transform);
+        waypoints.Add(newWaypoint);
+    }
+
+
+    private void LerpEntity()
+    {
+        if (play == true)
+        {
+            if (sphere == null)
+            {
+                if (GameObject.Find("Sphere"))
+                {
+                    sphere = GameObject.Find("Sphere");
+                }
+                else
+                {
+                    sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                }
+                currentPos = waypoints[0].transform.position;
+                nextPos = waypoints[1].transform.position;
+            }
+            
+
+            if (sphere.transform.position != nextPos && currentPos != null && nextPos != null)
+            {
+                if (repeat == true)
+                {
+                    distance = Vector3.Distance(currentPos, nextPos);
+                    repeat = false;
+                }
+                if (fraction < 1)
+                {
+                    fraction += Time.deltaTime / distance * speed;
+                    sphere.transform.position = Vector3.Lerp(currentPos, nextPos, fraction);
+                }
+            }
+            else if (waypoints.ElementAtOrDefault(points + 1) != null)
+            {
+                currentPos = waypoints[points].transform.position;
+                nextPos = waypoints[points + 1].transform.position;
+                fraction = 0;
+                points++;
+                repeat = true;
+            }
+            else
+            {
+                currentPos = waypoints[points].transform.position;
+                nextPos = waypoints[0].transform.position;
+                fraction = 0;
+                points = 0;
+                repeat = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        for (int i = 0; i < waypoints.Count; i++) {
+            if (waypoints.ElementAtOrDefault(i+1) != null) {
+                Gizmos.DrawLine(waypoints[i].transform.position, waypoints[i + 1].transform.position);
+            }
+            else {
+                Gizmos.DrawLine(waypoints[i].transform.position, waypoints[0].transform.position);
+            }
+        }
+    }
+}
